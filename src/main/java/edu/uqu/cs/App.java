f014a7bdb63402d2042298e4213529667c30b65f
Jana Alhakami <alhakamijana@gmail.com>
2023-03-22 15:59:13 +0300

    lab0 Jana Alhakami

--- src/main/java/edu/uqu/cs/App.java
+++ src/main/java/edu/uqu/cs/App.java
@@ -1,4 +1,5 @@
 package edu.uqu.cs;
+import java.util.*;
 /*
  * Lab 0 
  * CS 1312
@@ -23,6 +24,22 @@ public class App{
 *        1 Tweetle 3 TweetleBeetle 5 TweetlePoodle 7 TweetleBeetle 9 Tweetle 11
 *        TweetleBeetlePoodle 13 Tweetle 15 TweetleBeetle 17 TweetlePoodle 19 TweetleBeetle 
 */
+public static void twisters() {
+    for(int i=1;i<= 110;i++) {
+        if (i % 2==0){
+            System.out.print("Tweetle");
+            if(i % 4 == 0){
+                System.out.print("Beetle");
+            }
+            if( i % 6 == 0){
+                System.out.print("Boodle");
+            }
+        }
+        else{
+            System.out.print( " " + i + " " )
+        }
+    }
+}
 
 /**
 * Write a method named "phoneKeypad" that takes a string parameter (str)
@@ -42,17 +59,79 @@ public class App{
 * Output: 2255464228626
 *
 */
+public static void phoneKeypad( String str ){
+    str = str.toUpperCase();
+    String wtn = "" ;
+    for( int i = 0 ; i<= str.length() -1 ; i++){
+        switch(str.charAt(i)){
+             case 'A' :
+            case 'B' :
+            case 'C' :
+            wtn += 2 ;
+            break;
+            case 'D' :
+            case 'E' :
+            case 'F' :
+            wtn += 3 ;
+            break;
+            case 'G' :
+            case 'H' :
+            case 'I' :
+            wtn +=4 ;
+            break;
+            case 'J' :
+            case 'K' :
+            case 'L' :
+            wtn +=5 ;
+            break;
+            case 'M' :
+            case 'N' :
+            case 'O' :
+            wtn +=6 ;
+            break;
+            case 'P' :
+            case 'Q' :
+            case 'R' :
+            case 'S' :
+            wtn +=7 ;
+            break;
+            case 'T' :
+            case 'U' :
+            case 'V' :
+            wtn +=8 ;
+            break;
+            case 'W' :
+            case 'X' :
+            case 'Y' :
+            case 'z' :
+            wtn +=9 ;
+            break;
+
+        }
+    }
+    System.out.println(wtn);
+}
+           
+ 
+    
+    
+
  
     public static void main(String [] args) {
         
 
         /* Write your code here */
+        Scanner scnr = new Scanner(System.in);
      
        //call method twisters()
+       twisters();
        //prompt user to enter a string 
+       System.out.println("Enter a sentence:");
        //call method phoneKeypad(string)
+       phoneKeypad(scnr.nextLine());
 
 
     }
 
 }
+
